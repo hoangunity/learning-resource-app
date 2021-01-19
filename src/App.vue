dd3cdc0053a8e2cf26335a1505916efefa5ffd16
hoangunity <hoangdm.141196@gmail.com>
2021-01-19 23:22:14 +0700

    Adding & Styling Tabs

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,14 @@
 <template>
   <the-header title="RememberMe"></the-header>
-  <stored-resources :resources="storedResources"></stored-resources>
+  <the-resources></the-resources>
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 
 export default {
-  components: { StoredResources, TheHeader },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation.',
-          link: `https://vuejs.org`
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google...',
-          link: `https://google.com`
-        }
-      ]
-    };
-  }
+  components: { TheHeader, TheResources }
 };
 </script>
 
