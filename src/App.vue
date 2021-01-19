b34903219e4bed405c8b016dde610ef845c7f4a4
hoangunity <hoangdm.141196@gmail.com>
2021-01-19 21:25:27 +0700

    add and render LearningResource component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <ul></ul>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
 export default {
+  components: { LearningResource },
   data() {
     return {
-      storedResults: [
+      storedResources: [
         {
           id: 'official-guide',
           title: 'Official Guide',
